03abcf27187fc486a02af8f8a73d5b2a45cb5b80
danielsun888 <81004582+danielsun888@users.noreply.github.com>
2022-06-20 17:29:25 +0900

    t

--- OpenCV-Python-Tutorial/ch04-图片/颜色转换.py
+++ OpenCV-Python-Tutorial/ch04-图片/颜色转换.py
@@ -13,7 +13,12 @@ img = cv2.imread('messi5.jpg',cv2.IMREAD_UNCHANGED)#包括图像的 alpha 通道
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('image', img)#窗口会自动调整为图像大小
+cv2.imshow('gray', gray)#窗口会自动调整为图像大小
 
 temp = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)#灰色转RGB
-cv2.imshow('image', temp)#窗口会自动调整为图像大小
+cv2.imshow('temp', temp)#窗口会自动调整为图像大小
+k = cv2.waitKey(0)
+if k == 27:  # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'):  # wait for 's' key to save and exit
+    cv2.destroyAllWindows()
