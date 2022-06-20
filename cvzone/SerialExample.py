55009239fc28241ff6f910c864d9f616e1fdab91
danielsun888 <81004582+danielsun888@users.noreply.github.com>
2022-06-20 10:26:52 +0900

    t

--- cvzone/SerialExample.py
+++
@@ -1,9 +0,0 @@
-from cvzone.SerialModule import SerialObject
-import time
-
-mySerial = SerialObject("COM6", 9600, 1)
-while True:
-    mySerial.sendData([1, 1, 1, 1, 1])
-    time.sleep(2)
-    mySerial.sendData([0, 0, 0, 0, 0])
-    time.sleep(2)
