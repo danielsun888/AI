d8c525f00ca09d0bb91c06bd730ec9a88dba6579
danielsun888 <81004582+danielsun888@users.noreply.github.com>
2022-07-04 12:32:35 +0900

    t

--- ffm2.py
+++
@@ -1,6 +0,0 @@
-import ffmpy
-ff = ffmpy.FFmpeg(
-    inputs={'test.mp4': None},
-    outputs={'output.avi': None}
- )
-ff.run()
--- learning-nlp/chapter-10/bp/README.md
+++
@@ -1,6 +0,0 @@
-## requirements
-1. numpy
-2. python 2
-
-## 执行
-`python main.py`
