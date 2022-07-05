d4eaf51da4f09dc04e21d33267ac8672e04e0e01
danielsun888 <81004582+danielsun888@users.noreply.github.com>
2022-07-05 11:52:58 +0900

    t

--- Sklearn/1.3 Metrics Module/1.3.4.1.py
+++ Sklearn/1.3 Metrics Module/1.3.4.1.py
@@ -4,9 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 #----------------------------------------------------
 
-
+y_true = [2, 0, 2, 2, 0, 1]
+y_pred = [0, 0, 2, 2, 0, 2]
 #Calculating Confusion Matrix
-CM = confusion_matrix(y_test, y_pred)
+CM = confusion_matrix(y_true, y_pred)
 #print('Confusion Matrix is : \n', CM)
 
 # drawing confusion matrix
